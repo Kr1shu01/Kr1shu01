62994556686d613e03c5a619b666be8836cf1a10
kr1s <hxw2035143846@163.com>
2023-10-31 12:06:07 +0800

    Changes

---
+++ test/testguess.py
@@ -0,0 +1,13 @@
+import random
+num = random.randint(1,9)
+while True:
+    x = int(input("输入一个1-9的数字\n"))
+    if x > 9 or x < 1 :
+        print("Kidding me??")
+    elif x > num :
+        print("大了哦！再试一次。")
+    elif x < num :
+        print("小了哦！再试一次。")
+    else :
+        print("恭喜哦！猜对了呀！答案是"+str(num))
+        break
